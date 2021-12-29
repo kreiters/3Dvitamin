fbca57b478c4fda0aac78dfb49f5611981b66cb6
Kerry Kreiter <43457152+comm-scripter@users.noreply.github.com>
2021-12-29 14:38:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ const canvas = document.getElementById("renderCanvas"); // Get the canvas elemen
             const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});    
             
             capsule.position.y = 1.25;
+            capsule.rotate.y = Math.PI / 2.5;   
+                
             // GUI
             var plane = BABYLON.Mesh.CreatePlane("plane", 2);
             plane.parent = ground;
             plane.position.y = 2;
-            plane.position.z = -5;
+            plane.position.z = 5;
 
             var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
 
