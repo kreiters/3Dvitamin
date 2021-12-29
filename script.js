8af22e2a98c8ffaf33f7d0bb6e7ce72ccdd91adf
Kerry Kreiter <43457152+comm-scripter@users.noreply.github.com>
2021-12-29 13:45:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,24 @@ const canvas = document.getElementById("renderCanvas"); // Get the canvas elemen
 
             //BABYLON.MeshBuilder.CreateBox("box", {})
             const capsule = new BABYLON.MeshBuilder.CreateCapsule("capsule", {radius:0.5, capSubdivisions: 6, subdivisions:6, tessellation:36, height:2.5, orientation:BABYLON.Vector3.Forward()});
+            // GUI
+            var plane = BABYLON.Mesh.CreatePlane("plane", 2);
+            plane.parent = sphere;
+            plane.position.y = 2;
 
+            var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
+
+            var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Click to open vitamin");
+            button1.width = 1;
+            button1.height = 0.4;
+            button1.color = "white";
+            button1.fontSize = 50;
+            button1.background = "green";
+            button1.onPointerUpObservable.add(function() {
+                alert("you opened the vitamin!");
+            });
+            advancedTexture.addControl(button1);
+ 
             const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 8, new BABYLON.Vector3(0, 0, 0));
             camera.attachControl(canvas, true);
             const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
